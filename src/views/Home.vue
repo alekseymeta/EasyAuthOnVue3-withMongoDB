728a748be571f8e0c8d3b0edb69540c0bf2c1967
alekseymeta <metadnb@icloud.com>
2022-01-13 07:22:25 +0300

    clear

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,14 @@
     <Spinner v-if="loading"/>
     <div v-else>
       <h1>Hello</h1>
-      <blocks/>
     </div>
 </template>
 
 <script>
 
-import blocks from "../components/blocks";
-
 export default {
   name: 'Home',
-  components: {blocks},
+  components:{ Spinner },
   data() {
     return {
       loading: false
