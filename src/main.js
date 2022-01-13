4a33b1333ed381f72c879a16c3d743275b42b958
alekseymeta <metadnb@icloud.com>
2022-01-13 09:49:19 +0300

    bag fix

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// import messagePlugin from './utils/messagePlugin'
-// import {mongoConfig} from "./mongoConfig";
+
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
